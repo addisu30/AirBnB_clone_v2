6803f922bf4f87b3f16bd9b46f0a203411b1fba2
addisu30 <dabaleaddisu21@outlook.com>
2023-01-04 15:39:52 +0300

    cleaning

---
+++ 100-clean_web_static.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2023 Jan 04 14:21:54
+@author: Addisu Dabale
+"""
+from fabric.api import local, put, run, env, cd, lcd
+from datetime import datetime
