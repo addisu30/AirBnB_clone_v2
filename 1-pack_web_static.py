ee3ab138c84c723704cc8a44a9e12b1ca7d70576
addisu30 <dabaleaddisu21@outlook.com>
2023-02-20 12:12:33 +0300

    updating

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on 2023 Jan Wed 04 12:45:54
-@author: addisu dabale
-"""
+# fabfile to generate archive file
 from fabric.api import local, env
 from datetime import datetime
 
