8ea33462bf9eb146df9571f8febf7b27e6be6ecf
addisu30 <dabaleaddisu21@outlook.com>
2023-04-23 22:42:37 +0300

    c is fun updating

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """Starts a Flask web application.
 
@@ -31,23 +30,3 @@ def text(text=None):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-=======
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/', strict_slashes=False)
-def display_hello_hbnb():
-    return ("Hello HBNB")
-
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    return ("HBNB")
-
-@app.route('/c/<text>')
-def display_text(text):
-    return ("C %s" % text)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
->>>>>>> 20222ba05ffb64fd4dc0d5bf49800b20d555745d
