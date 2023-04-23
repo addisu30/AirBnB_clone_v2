f72c25e3fa18dc7fefc9ba4d614226bacc3880d0
addisu30 <dabaleaddisu21@outlook.com>
2023-04-23 22:24:29 +0300

    updating

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """Starts a Flask web application.
 
@@ -18,17 +17,4 @@ def hello():
 
 
 if __name__ == '__main__':
-=======
-from flask import Flask
-
-
-app = Flask(__name__)
-
-@app.route('/', strict_slashes=False)
-def Display_hello():
-    return ('“Hello HBNB!”')
-
-
-if __name__ == "__main__":
->>>>>>> 20222ba05ffb64fd4dc0d5bf49800b20d555745d
     app.run(host='0.0.0.0', port=5000)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """Starts a Flask web application.
 
@@ -25,19 +24,17 @@ def hbnb():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-=======
-from flask import Flask
 
-app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def display_hello_hbnb():
     return ("Hello HBNB")
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     return ("HBNB")
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
->>>>>>> 20222ba05ffb64fd4dc0d5bf49800b20d555745d
