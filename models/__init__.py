b6a1be617491e17970dcb205591ae29a87fb3df1
addisu30 <105362027+addisu30@users.noreply.github.com>
2023-04-23 23:27:43 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """Instantiates a storage object.
 -> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
    instantiates a database storage engine (DBStorage).
@@ -15,19 +14,3 @@ else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
-=======
-"""This module instantiates an object of class FileStorage"""
-import os
-from sqlalchemy.orm import relationship, sessionmaker
-from sqlalchemy import create_engine, String
-
-
-if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    from .engine import DBStorage
-    storage = DBStorage()
-    storage.reload()
-else:
-    from .engine import FileStorage
-    storage = FileStorage()
-    storage.reload()
->>>>>>> 20222ba05ffb64fd4dc0d5bf49800b20d555745d
